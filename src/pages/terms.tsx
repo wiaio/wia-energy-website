88485f6a7e7b355ff5fd2aff825065a91572981d
naticampiglia <99177631+naticampiglia@users.noreply.github.com>
2024-02-04 16:33:25 +0800

    Terms

---
+++ src/pages/terms.tsx
@@ -0,0 +1,399 @@
+import * as React from 'react';
+
+import Layout from '../components/layout';
+
+const Terms = () => (
+  <Layout>
+    <div id='#terms' className=''>
+      <div className='container mx-auto'>
+        <div className='flex flex-col relative md:py-24 py-16'>
+          <div className='md:col-span-12 flex flex-col items-center mb-10'>
+            <h2 className='lg:text-display-lg text-display-md font-semibold pb-14 text-center lg:px-24'>
+              Terms & Conditions
+            </h2>
+
+            <div className='section--terms sm:p-10'>
+              <h5>Terms of Service ("Terms")</h5>
+              <p>Last updated: May 23, 2018</p>
+
+              <p>
+                Please read these Terms of Service ("Terms", "Terms of Service")
+                carefully before using the https://www.wia.io website (the
+                "Service") operated by Wia Limited ("us", "we", or "our").
+              </p>
+
+              <p>
+                Your access to and use of the Service is conditioned on your
+                acceptance of and compliance with these Terms. These Terms apply
+                to all visitors, users and others who access or use the Service.
+              </p>
+
+              <p>
+                <strong>
+                  By accessing or using the Service you agree to be bound by
+                  these Terms. If you disagree with any part of the terms then
+                  you may not access the Service.
+                </strong>
+              </p>
+
+              <p>
+                <strong>Purchases</strong>
+              </p>
+
+              <p>
+                <strong>
+                  You are encouraged to familiarise yourself with your rights
+                  contained within the Sale of Goods Act 1979, Unfair Contract
+                  Terms Act 1977 and the Unfair Terms in Consumer Contracts
+                  Regulations 1999.
+                </strong>
+              </p>
+
+              <p>
+                If you wish to purchase any product or service made available
+                through the Service ("Purchase"), you may be asked to supply
+                certain information relevant to your Purchase including, without
+                limitation, your credit card number, the expiration date of your
+                credit card, your billing address, and your shipping
+                information.
+              </p>
+
+              <p>
+                You represent and warrant that: (i) you have the legal right to
+                use any credit card(s) or other payment method(s) in connection
+                with any Purchase; and that (ii) the information you supply to
+                us is true, correct and complete. You expressly agree that Wia
+                Limited is not responsible for any loss or damage arising from
+                the submission of false or inaccurate information.
+              </p>
+
+              <p>
+                By submitting such information, you grant us the right to
+                provide the information to third parties for purposes of
+                facilitating the completion of Purchases.
+              </p>
+
+              <p>
+                We reserve the right to refuse or cancel your order at any time
+                for certain reasons including but not limited to: product or
+                service availability, errors in the description or price of the
+                product or service, error in your order or other reasons. You
+                expressly agree that Wia Limited cannot accept any liability for
+                loss or damage arising out of such cancellation.
+              </p>
+
+              <p>
+                We reserve the right to refuse or cancel your order if fraud or
+                an unauthorised or illegal transaction is suspected.
+              </p>
+
+              <p>
+                <strong>Availability, Errors and Inaccuracies</strong>
+              </p>
+
+              <p>
+                We are constantly updating our offerings of products and
+                services on the Service. The products or services available on
+                our Service may be mispriced, described inaccurately, or
+                unavailable, and we may experience delays in updating
+                information on the Service and in our advertising on other web
+                sites. You expressly agree that any such offer of a product or
+                service does not constitute a legal offer capable of attracting
+                legal consequences.
+              </p>
+
+              <p>
+                We cannot and do not guarantee the accuracy or completeness of
+                any information, including prices, product images,
+                specifications, availability, and services. We reserve the right
+                to change or update information and to correct errors,
+                inaccuracies, or omissions at any time without prior notice.
+                Section "Availability, Errors and Inaccuracies" is without
+                prejudice to existing statutory rights.
+              </p>
+
+              <p>
+                <strong>Content</strong>
+              </p>
+
+              <p>
+                Our Service allows you to post, link, store, share and otherwise
+                make available certain information, text, graphics, videos, or
+                other material ("Content"). You are responsible for the Content
+                that you post to the Service, including its legality,
+                reliability, and appropriateness.
+              </p>
+
+              <p>
+                By posting Content to the Service, you grant us the right and
+                license to use, modify, publicly perform, publicly display,
+                reproduce, and distribute such Content on and through the
+                Service. You retain any and all of your rights to any Content
+                you submit, post or display on or through the Service and you
+                are responsible for protecting those rights.
+              </p>
+
+              <p>
+                You represent and warrant that: (i) the Content is yours (you
+                own it) or you have the right to use it and grant us the rights
+                and license as provided in these Terms, and (ii) the posting of
+                your Content on or through the Service does not violate the
+                privacy rights, publicity rights, copyrights, contract rights or
+                any other rights of any person.
+              </p>
+
+              <p>
+                <strong>Accounts</strong>
+              </p>
+
+              <p>
+                When you create an account with us, you must provide us
+                information that is accurate, complete, and current at all
+                times. Failure to do so constitutes a breach of the Terms, which
+                may result in immediate termination of your account on our
+                Service.
+              </p>
+
+              <p>
+                You are responsible for safeguarding the password that you use
+                to access the Service and for any activities or actions under
+                your password, whether your password is with our Service or a
+                third-party service.
+              </p>
+
+              <p>
+                You agree not to disclose your password to any third party. You
+                must notify us immediately upon becoming aware of any breach of
+                security or unauthorized use of your account.
+              </p>
+
+              <p>
+                You may not use as a username the name of another person or
+                entity or that is not lawfully available for use, a name or
+                trade mark that is subject to any rights of another person or
+                entity other than you without appropriate authorization, or a
+                name that is otherwise offensive, vulgar or obscene. You
+                expressly agree that we cannot be held liable for any loss or
+                damage arising out of any misrepresentations you make in this
+                regard.
+              </p>
+
+              <p>
+                <strong>Intellectual Property</strong>
+              </p>
+              <p>
+                The Service and its original content (excluding Content provided
+                by users), features and functionality are and will remain the
+                exclusive property of Wia Limited and its licensors. The Service
+                is protected by copyright, trademark, and other laws of both the
+                United Kingdom and foreign countries. Our trademarks and trade
+                dress may not be used in connection with any product or service
+                without the prior written consent of Wia Limited.
+              </p>
+
+              <p>
+                When you upload content, you give to Wia Limited a worldwide,
+                non-exclusive, royalty-free, transferable licence (with right to
+                sub-licence) to use, reproduce, distribute, prepare derivative
+                works of, display, and perform that Content in connection with
+                the provision of the Service and otherwise in connection with
+                the provision of the Service and Wia Limited business.
+              </p>
+
+              <p>
+                <strong>Links To Other Web Sites</strong>
+              </p>
+
+              <p>
+                Our Service may contain links to third-party web sites or
+                services that are not owned or controlled by Wia Limited.
+              </p>
+
+              <p>
+                Wia Limited has no control over, and assumes no responsibility
+                for, the content, privacy policies, or practices of any third
+                party web sites or services. You further acknowledge and agree
+                that Wia Limited shall not be responsible or liable, directly or
+                indirectly, for any damage or loss caused or alleged to be
+                caused by or in connection with use of or reliance on any such
+                content, goods or services available on or through any such web
+                sites or services.
+              </p>
+
+              <p>
+                We strongly advise you to read the terms and conditions and
+                privacy policies of any third-party web sites or services that
+                you visit.
+              </p>
+
+              <p>
+                <strong>Termination</strong>
+              </p>
+
+              <p>
+                We may terminate or suspend your account immediately, without
+                prior notice or liability, for any reason whatsoever, including
+                without limitation if you breach the Terms.
+              </p>
+
+              <p>
+                Upon termination, your right to use the Service will immediately
+                cease. If you wish to terminate your account, you may simply
+                discontinue using the Service.
+              </p>
+
+              <p>
+                All provisions of the Terms which by their nature should survive
+                termination shall survive termination, including, without
+                limitation, ownership provisions, warranty disclaimers,
+                indemnity and limitations of liability.
+              </p>
+
+              <p>
+                <strong>Indemnification</strong>
+              </p>
+
+              <p>
+                You agree to defend, indemnify and hold harmless Wia Limited and
+                its licensee and licensors, and their employees, contractors,
+                agents, officers and directors, from and against any and all
+                claims, damages, obligations, losses, liabilities, costs or
+                debt, and expenses (including but not limited to attorney's
+                fees), resulting from or arising out of a) your use and access
+                of the Service, by you or any person using your account and
+                password; b) a breach of these Terms, or c) Content posted on
+                the Service.
+              </p>
+
+              <p>
+                <strong>Limitation Of Liability</strong>
+              </p>
+
+              <p>
+                In no event shall Wia Limited, nor its directors, employees,
+                partners, agents, suppliers, or affiliates, be liable for any
+                indirect, incidental, special, consequential or punitive
+                damages, including without limitation, loss of profits, data,
+                use, goodwill, or other intangible losses, resulting from (i)
+                your access to or use of or inability to access or use the
+                Service; (ii) any conduct or content of any third party on the
+                Service; (iii) any content obtained from the Service; and (iv)
+                unauthorized access, use or alteration of your transmissions or
+                content, whether based on warranty, contract, tort (including
+                negligence) or any other legal theory, whether or not we have
+                been informed of the possibility of such damage, and even if a
+                remedy set forth herein is found to have failed of its essential
+                purpose.
+              </p>
+
+              <p>
+                <strong>Disclaimer</strong>
+              </p>
+
+              <p>
+                Your use of the Service is at your sole risk. The Service is
+                provided on an "AS IS" and "AS AVAILABLE" basis. The Service is
+                provided without warranties of any kind, whether express or
+                implied, including, but not limited to, implied warranties of
+                merchantability, fitness for a particular purpose,
+                non-infringement or course of performance.
+              </p>
+
+              <p>
+                Wia Limited its subsidiaries, affiliates, and its licensors do
+                not warrant that a) the Service will function uninterrupted,
+                secure or available at any particular time or location; b) any
+                errors or defects will be corrected; c) the Service is free of
+                viruses or other harmful components; or d) the results of using
+                the Service will meet your requirements.
+              </p>
+
+              <p>
+                <strong>Exclusions</strong>
+              </p>
+
+              <p>
+                Without limiting the generality of the foregoing and
+                notwithstanding any other provision of these terms, under no
+                circumstances will Wia Limited ever be liable to you or any
+                other person for any indirect, incidental, consequential,
+                special, punitive or exemplary loss or damage arising from,
+                connected with, or relating to your use of the Service, these
+                Terms, the subject matter of these Terms, the termination of
+                these Terms or otherwise, including but not limited to personal
+                injury, loss of data, business, markets, savings, income,
+                profits, use, production, reputation or goodwill, anticipated or
+                otherwise, or economic loss, under any theory of liability
+                (whether in contract, tort, strict liability or any other theory
+                or law or equity), regardless of any negligence or other fault
+                or wrongdoing (including without limitation gross negligence and
+                fundamental breach) by Wia Limited or any person for whom Wia
+                Limited is responsible, and even if Wia Limited has been advised
+                of the possibility of such loss or damage being incurred.
+              </p>
+
+              <p>
+                <strong>Governing Law</strong>
+              </p>
+
+              <p>
+                These Terms shall be governed and construed in accordance with
+                the laws of England and Wales, without regard to its conflict of
+                law provisions.
+              </p>
+
+              <p>
+                Our failure to enforce any right or provision of these Terms
+                will not be considered a waiver of those rights. If any
+                provision of these Terms is held to be invalid or unenforceable
+                by a court, the remaining provisions of these Terms will remain
+                in effect. These Terms constitute the entire agreement between
+                us regarding our Service, and supersede and replace any prior
+                agreements we might have between us regarding the Service.
+              </p>
+
+              <p>
+                <strong>Changes</strong>
+              </p>
+
+              <p>
+                We reserve the right, at our sole discretion, to modify or
+                replace these Terms at any time. If a revision is material we
+                will try to provide at least 30 days notice prior to any new
+                terms taking effect. What constitutes a material change will be
+                determined at our sole discretion.
+              </p>
+
+              <p>
+                By continuing to access or use our Service after those revisions
+                become effective, you agree to be bound by the revised terms. If
+                you do not agree to the new terms, you must stop using the
+                service.
+              </p>
+
+              <p>
+                <strong>Privacy Policy and Cookie Policy</strong>
+              </p>
+
+              <p>
+                Please refer to our Privacy Policy and Cookies Policy. You agree
+                that they constitute part of these terms. You must read our
+                Privacy Policy and Cookies Policy before you use the Service.
+              </p>
+
+              <p>
+                <strong>Contact Us</strong>
+              </p>
+
+              <p>
+                If you have any questions about these Terms, please contact us.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
+);
+
+export default Terms;

45c5fba60529a52a0c1921685623c37528145a04
naticampiglia <99177631+naticampiglia@users.noreply.github.com>
2024-02-06 22:12:13 +1100

    Fix redirect wia-logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,9 +8,9 @@ const Header = () => {
       <div className='container mx-auto'>
         <div className='flex py-6 justify-between items-center'>
           <div className='flex flex-row gap-8 items-center'>
-            <Link to='/'>
-              <a className='navbar-brand'></a>
-            </Link>
+            <a className='navbar-brand'>
+              <Link to='/' className='navbar-link'></Link>
+            </a>
           </div>
           <div className='flex flex-row gap-6'>
             <Button label='Contact Us' link='mailto:sales@wia.io' />

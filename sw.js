092f884f2a05854b16dadcf709c0d07b6cb98026
naticampiglia <naticampiglia@users.noreply.github.com>
2024-02-06 03:47:51 +0000

    deploy: b3f5845c116c8fe8b7f9546177aeedf1b150e182

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c81f699dff7f5b6f6030.js"
+    "url": "webpack-runtime-89842f3dc56f6b79b165.js"
   },
   {
     "url": "framework-9d9451af489ba4de012e.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8badd8596d9a99184d199be11b514bdc"
+    "revision": "8e9cd7d31763873b1fcf25c2d03c5ed4"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
